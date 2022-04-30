495fdfdc232669a4364b3c7febbbd72dd76de1c6
gensan-64 <genyuu6240@gmail.com>
2022-04-30 17:48:46 +0900

    L23ファイル作成

---
+++ chap2/L23/recive.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>出力結果</title>
+</head>
+<body>
+    <?php
+    // print_r($_POST);
+    echo htmlspecialchars($_POST['recipe_name'], ENT_QUOTES);
+    echo '<br>';
+    echo match ($_POST['category']) {
+        '1' => '和食',
+        '2' => '中華',
+        '3' => '洋食',
+    } . '<br>';
+    echo match ($_POST['difficulty']) {
+        '1' => '簡単',
+        '2' => '普通',
+        '3' => '難しい',
+    } . '<br>'
+    ?>
+</body>
+</html>
