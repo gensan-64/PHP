f20747c1de914297fc75cd4eac60762fe04ed1b5
gensan-64 <genyuu6240@gmail.com>
2022-05-17 21:54:42 +0900

    L40ファイル作成

---
+++ chap4/L40/detail.php
@@ -0,0 +1,14 @@
+<?php
+$user = '●●●';
+$pass = '●●●';
+if (empty($_GET['id'])) {
+    echo 'IDを正しく入力してください。';
+    exit;
+}
+try {
+    $id = (int)$_GET['id'];
+    var_dump($id);
+} catch (PDOException $e) {
+    echo 'エラー発生: ' . htmlspecialchars($e->getMessage(), ENT_QUOTES) . '<br>';
+    exit;
+}
